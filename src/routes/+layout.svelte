186c5278d808fc48fa9d0c785f391547a12675c7
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-11-18 17:30:19 +0000

    added header footer and navbar formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,53 +8,87 @@
 </script>
 
 <GdprBanner heading="GDPR Cookie Notice" cookieName="AnotherCookie" description="Please let me store cookies on yor computer." on:analytics={initAnalytics} />
+<div class="page">
+<div class="header">
+	<h1>1stcall's sdm command line generator</h1>
+	<nav class="nav">
+		<ul>
+			<li><a href="/">home</a></li>
+			<li><a href="/oslist">OS list</a></li>
+			<li><a href="/switches">switches</a></li>
+			<li style="float:right"><a href="/about">about</a></li>
+		</ul>
+	</nav>
+</div>
 
-<nav class="nav">
-	<ul>
-		<li><a href="/">home</a></li>
-    	<li><a href="/oslist">OS list</a></li>
-		<li><a href="/switches">switches</a></li>
-		<li><a href="/about">about</a></li>
-	</ul>
-</nav>
+<div class="clear"></div>
+<div class="clear"></div>
+<div class="clear"></div>
 
 <div class="content">
 	<slot />
 </div>
 
+<div class="clear"></div>
+
 <div class="footer">
-	<p>Footer</p>
+	<p>
+		Copyright &copy; 2023 1stcall Information Technology.  All rights researved.
+	</p>
 </div>
-  
+</div>
+
 <style>
-	.content {
-		margin: 0em;
-		padding: 1em;
-		background-color: #fec89a;
+	.page {
+		background: wheat;
 	}
 	.nav ul {
 		list-style-type: none;
-  		margin: 0em;
-  		padding: 1em;
-		background-color: #457b9d;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #333;
 	}
 	.nav ul li {
-		margin: 2em;
-		padding: 1em;
-		background-color: #264653;
-		display: inline;
+		float: left;
+		border-left: none;
+		border-right: 1px solid #bbb;
+	}
+	.nav ul li:last-child {
+		border-right: none;
+		border-left: 1px solid #bbb;
 	}
 	.nav ul li a {
-    	color: #457b9d;
-    	text-decoration: none;
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+	}
+	.nav ul li a:hover {
+		background-color: #111;
+	}
+	.header {
+		left: 0.5rem;
+		right: 0.5rem;
+   		top: 0rem;
+		text-align: center;
+		position: fixed;
+		background: wheat 0 ;
 	}
 	.footer {
-		position: fixed;
-   		left: 0;
-   		bottom: 0;
-   		width: 100%;
-	    color: white;
+   		left: 0.5rem;
+		right: 0.5rem;
+   		bottom: 0rem;
    		text-align: center;
-	   	background-color: #fec89a;
+		position: fixed;
+		background: wheat;
+		border-top: 1px solid #bbb;
+		height: 4.5rem;
+		vertical-align: bottom;
+	}
+	.clear {
+		clear: both;
+		height: 4.5rem;
 	}
 </style>
