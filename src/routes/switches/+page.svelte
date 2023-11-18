c68bc0088ca1e58a3b5747d697a8321010ade1b7
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-11-18 17:30:58 +0000

    added some page formating

--- src/routes/switches/+page.svelte
+++ src/routes/switches/+page.svelte
@@ -5,22 +5,46 @@
 <h1>Switches list</h1>
 <p>This is the Switches list page for sdmui.</p>
 
-<ul>
-	{#each data.switches as { name, description, selected, enabled, hasArgument, argumentType, argumentValue }}
-		<li><p>	{name} 
-			<input id="{ name }" type="checkbox" bind:checked="{ selected }" enabled="{ enabled }">
-			{#if { hasArgument }}
-				{#if { argumentType } == "text" }
-					<input type="text" disabled="{ !selected }" bind:value="{ argumentValue }">
-				{:else if { argumentType } == "file" }
-					<input type="file" disabled="{ !selected }" bind:value="{ argumentValue }">
-				{:else if { argumentType } == "ipAddress" }
-					<input type="text" disabled="{ !selected }" bind:value="{ argumentValue }" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$">
-				{:else if { argumentType } == "multiFile" }
-					<input type="file" disabled="{ !selected }" bind:value="{ argumentValue }" multiple>
-				{/if}
-			{/if} 
-		</p></li>
-		<p>{description}</p>
+<form class="switches">
+	{#each data.switches as { name, description, selected, editable, hasArgument, argumentType, argumentValue }}
+		<div class="row">
+			<td class="firstcol"><label for="{ name }">{ name }</label></td>
+			<td class="col"><input id="{ name }" type="checkbox" bind:checked="{ selected }" disabled="{ !editable }"></td>
+			{#if hasArgument == true}
+				<td class="col">
+					{#if argumentType == "text"}
+						<input type="text" disabled="{ !selected }" bind:value="{ argumentValue }">
+					{:else if argumentType == "file"}
+						<input type="file" disabled="{ !selected }" bind:value="{ argumentValue }">
+					{:else if argumentType == "ipAddress"}
+						<input type="text" disabled="{ !selected }" bind:value="{ argumentValue }" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$">
+					{:else if argumentType == "multiFile"}
+						<input type="file" disabled="{ !selected }" bind:value="{ argumentValue }" multiple>
+					{/if}
+				</td>
+			{:else}
+				<td> </td>
+			{/if}
+			<p>{ description }</p>
+		</div>
 	{/each}
-</ul>
+</form>
+
+<style>
+	.switches {
+		display: table;
+		max-width: 100%;
+	}
+	.row {
+		display: table-row;
+	}
+	.col {
+		display: table-cell;
+		padding-top: 10px;
+	}
+	.firstcol {
+		display: table-cell;
+		white-space: nowrap;
+		padding-top: 10px;
+	}
+</style>
