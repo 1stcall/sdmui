979f2cbfada8de4d833188e2d4a9d8445290d4c4
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-11-26 10:26:12 +0000

    more work on dockerisation

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ docker stop sdmui --time 0
 docker system prune --all --force
 
 npm run build
+cp ./package*.json ./build/
+cp ./Dockerfile ./build/
+cp ./compose.yaml ./build/
 
-docker build . -t sdmui
-docker run --detach --publish 80:80 --name sdmui -t sdmui
-sleep 2
-docker logs sdmui 
+#docker build . -t sdmui
+#docker run --detach --publish 80:80 --name sdmui -t sdmui
+#sleep 2
+#docker logs sdmui 
